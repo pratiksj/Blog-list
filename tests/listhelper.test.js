d14240c4439fea3d2958b3136812bb868957d828
pratiksj <pratiksharai728@gmail.com>
2022-09-20 15:53:24 +0545

    feat: tested initial function

--- tests/listhelper.test.js
+++ tests/listhelper.test.js
@@ -6,3 +6,99 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  test("when list has only one blog, equals the likes of that", () => {
+    const listWithOneBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("when list has more than one blog, sum all the likes ", () => {
+    const listWithOneBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(12);
+  });
+});
+
+describe("favouriteBlog", () => {
+  test("when list has most vote", () => {
+    const favoriteBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 12,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.favouriteBlog(favoriteBlog);
+    expect(result).toStrictEqual({
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    });
+  });
+
+  test("when list has highest vote", () => {
+    const favoriteBlog = [
+      {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0,
+      },
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+      {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0,
+      },
+    ];
+    const result = listHelper.favouriteBlog(favoriteBlog);
+    expect(result).toEqual({
+      title: "React patterns",
+      author: "Michael Chan",
+      likes: 7,
+    });
+  });
+});
--- utills/list_helper.js
+++ utills/list_helper.js
@@ -2,6 +2,84 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (like) => {
+  return like.reduce((previous, currVal) => previous + currVal.likes, 0);
+};
+
+// const favoriteBlog = (blog) => {
+//   console.log(blog, "hellow");
+//   let result = blog.reduce((pre, curr) => {
+//     if (blog.length === 1) {
+//       pre = {
+//         title: curr.title,
+//         author: curr.author,
+//         likes: curr.likes,
+//       };
+//     }
+//     if (curr.likes >= pre.likes) {
+//       pre = {
+//         title: curr.title,
+//         author: curr.author,
+//         likes: curr.likes,
+//       };
+//     } else {
+//       pre = {
+//         title: pre.title,
+//         author: pre.author,
+
+//         likes: pre.likes,
+//       };
+//     }
+
+//     console.log("i am in", pre);
+//     return pre;
+//   }, {});
+//   return result;
+// };===>
+// const favouriteBlog = (blogs) => {
+//   if (!blogs.length) return null;
+
+//   let maxLikes = blogs.reduce((a, b) => {
+//     if (b.likes > a) return b.likes;
+//     else return a;
+//   }, 0);
+
+//   let favBlog = blogs.find((blog) => {
+//     return blog.likes === maxLikes;
+//   });
+
+//   return { title: favBlog.title, author: favBlog.author, likes: favBlog.likes } ====>solutin
+// };
+const favouriteBlog = (blogs) => {
+  const mostliked = blogs.reduce((previousValue, currentValue) => {
+    if (blogs.length === 1) {
+      previousValue = {
+        title: currentValue.title,
+        author: currentValue.author,
+        likes: currentValue.likes,
+      };
+    }
+    if (previousValue.likes >= currentValue.likes) {
+      previousValue = {
+        title: previousValue.title,
+        author: previousValue.author,
+        likes: previousValue.likes,
+      };
+    } else {
+      previousValue = {
+        title: currentValue.title,
+        author: currentValue.author,
+        likes: currentValue.likes,
+      };
+    }
+    console.log(previousValue);
+    return previousValue;
+  }, {});
+  return mostliked;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favouriteBlog,
 };
