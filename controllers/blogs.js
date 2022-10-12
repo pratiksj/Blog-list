7b2a57a637143779597ea30f1728923a6bd3b229
pratiksj <pratiksharai728@gmail.com>
2022-10-12 22:27:32 +0545

    extended the errorhandled middler ware incase of faulty token

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -25,29 +25,49 @@ blogsRouter.get("/:id", async (request, response, next) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response, next) => {
-  const body = request.body;
-
-  const user = await User.findById(body.userId);
-
-  if (!body.likes) {
-    body.likes = 0;
-  }
-  if (!(body.title || body.url)) {
-    response.status(400).end();
-  }
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id,
-  });
   try {
-    const newBlog = await blog.save();
-    user.blogs = user.blogs.concat(newBlog._id);
-    await user.save();
-    response.status(201).json(newBlog);
+    const body = request.body;
+
+    //const user = await User.findById(body.userId);
+
+    if (!body.likes) {
+      body.likes = 0;
+    }
+    //const blog = await Blog.find({title:body.title})
+
+    if (!(body.title || body.url)) {
+      response.status(400).json({ error: "tittle and url are required" });
+    } else {
+      const token = getTokenFrom(request);
+      const decodedToken = jwt.verify(token, process.env.SECRET);
+      if (!decodedToken.id) {
+        return response.status(401).json({ error: "token missing or invalid" });
+      }
+      const user = await User.findById(decodedToken.id);
+      if (!user) {
+        response.status(401).json({ error: "token missing or invalid" });
+      }
+      const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id,
+      });
+      const newBlog = await blog.save();
+      user.blogs = user.blogs.concat(newBlog._id);
+      await user.save();
+      response.status(201).json(newBlog);
+    }
   } catch (error) {
     next(error);
   }
