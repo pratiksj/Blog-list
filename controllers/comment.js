df01765cd0bebcba5e4e8e61595e4c7b143134e3
pratiksj <pratiksharai728@gmail.com>
2023-01-08 11:53:48 +0545

    feat: specify comment for blog in the controller

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,17 +1,27 @@
 const Comment = require("../model/comment");
 const commentRouter = require("express").Router();
 
-commentRouter.get("/:id/comments", async (request, response) => {
-  const comments = await Comment.find();
-  response.send(comments);
+commentRouter.get("/:id/comments", async (request, response, next) => {
+  try {
+    const comments = await Comment.find({ blog_id: request.params.id });
+    //const comments = await Comment.find();
+    response.send(comments);
+  } catch (error) {
+    next(error);
+  }
 });
 
-commentRouter.post("/:id/comments", async (request, response) => {
-  const remark = new Comment({
-    comment: request.body.comment,
-  });
-  const commentNew = await remark.save();
-  response.send(commentNew);
+commentRouter.post("/:id/comments", async (request, response, next) => {
+  try {
+    const remark = new Comment({
+      comment: request.body.comment,
+      blog_id: request.params.id,
+    });
+    const commentNew = await remark.save();
+    response.send(commentNew);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = commentRouter;
--- model/comment.js
+++ model/comment.js
@@ -1,23 +1,33 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+//const Schema = mongoose.Schema;
 
 const commentSchema = new mongoose.Schema({
   comment: String,
-  blog: {
-    type: Schema.Types.ObjectId,
-    ref: "Blog",
-  },
+  //   blog: {
+  //     type: Schema.Types.ObjectId,
+  //     ref: "Blog",
+  //   },
+  blog_id: String,
 });
+
+// commentSchema.set("toJSON", {
+//   //this is just format
+//   transform: (document, returnedObject) => {
+//     //returnedObject ma database bata return vako object hunxa
+//     returnedObject.id = returnedObject._id.toString();
+//     delete returnedObject._id;
+//     delete returnedObject.__v;
+//   },
+// });
 
 commentSchema.set("toJSON", {
-  //this is just format
-  transform: (document, returnedObject) => {
-    //returnedObject ma database bata return vako object hunxa
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
   },
 });
 
-const Comment = mongoose.model("Comment", commentSchema);
-module.exports = Comment;
+// const Comment = mongoose.model("Comment", commentSchema); //trying to import commentshema without this part
+// module.exports = Comment;
+module.exports = mongoose.model("Comment", commentSchema);
